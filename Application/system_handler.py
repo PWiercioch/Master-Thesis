2aec0b8c99e7d67ac463c98eeb162f4be4a2ab15
Pwiercioch <pawiercioch@gmail.com>
2022-11-03 20:45:56 +0100

    Abstract object detection from main loop in system_handler

--- Application/system_handler.py
+++ Application/system_handler.py
@@ -5,6 +5,7 @@ from models.deep_sort.depsort import DeepSort
 from models.model_loader import ModelLoader
 from video_reader import VideoReader
 import tensorflow as tf
+import numpy as np
 
 
 class SystemHandler:
@@ -22,6 +23,7 @@ class SystemHandler:
         disnet : distance estimation model instance
         midas : depth estimation model instance
         tracker : object tracker instance
+        od_threshold : object detection probability threshold
 
     """
     def __init__(self, model_loader: ModelLoader) -> None:
@@ -30,6 +32,27 @@ class SystemHandler:
         self.disnet = DisNet(model_loader.distance_model)
         self.midas = MiDas(model_loader.depth_model)
         self.tracker = DeepSort()
+
+        self.od_threshold = 0.6  # maybe provide a parameter
+
+    def __get_detections(self, frame: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Method for getting bounding boxes and object classes from frame, when detection probability is high enough
+
+            :param frame: video frame for object detection
+
+            :return: detected objects bounding boxes and classes
+        """
+        img_detetect, detections = self.detector.predict(frame)
+
+        ind = detections['detection_scores'] > self.od_threshold
+        boxes = detections['detection_boxes'][ind].numpy()
+        classes = detections['detection_classes'][ind].numpy()
+
+        boxes = boxes * self.od_resolution[0]
+        boxes = boxes.astype(int)
+
+        return boxes, classes
 
     def process_video(self, path: str, disp_res: tuple[int, int]) -> None:
         """
@@ -47,23 +70,12 @@ class SystemHandler:
             with tf.device("/device:GPU:0"):
 
                 while True:
-                    frame = video.get_frame()
+                    ret, frame = video.get_frame()
 
-                    if frame[0]:  # break if no valid frame is retrieved
+                    if ret:  # break if no valid frame is retrieved
                         break
 
-                    frame = frame[1]
-
-                    # TODO - use wrapper here
-                    img_detetect, detections = self.detector.predict(frame)
-
-                    ind = detections['detection_scores'] > 0.6
-                    boxes = detections['detection_boxes'][ind].numpy()
-                    classes = detections['detection_classes'][ind].numpy()
-
-                    boxes = boxes * 320
-                    boxes = boxes.astype(int)
-
+                    boxes, classes = self.__get_detections(frame)
                     reader.annonate_image(boxes, classes)
 
                     if reader.show_frame():  # break on user interrupt
